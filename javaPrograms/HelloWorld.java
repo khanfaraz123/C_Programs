75326e9a6feb9c99b1af4fe0a5cc3dea3aedb6d4
khanfaraz123 <52497272+khanfaraz123@users.noreply.github.com>
2024-09-20 19:07:18 +0530

    Add files via upload
    types to declaring integer type

---
+++ javaPrograms/HelloWorld.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        short myShort= 32767;
+	int myInt = 2147483647
+	float f1 = 35e3f;
+	double d1 = 12E4d;
+
+        System.out.println(myByte);
+        System.out.println(myShort);
+	System.out.println(myInt);
+	System.out.println(f1);
+	System.out.println(d1);
+    }
+}
