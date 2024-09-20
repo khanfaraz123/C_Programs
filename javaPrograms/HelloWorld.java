0da92768712f03f8287a7130c3145409dfaa62fd
Mohd Faraz <52497272+khanfaraz123@users.noreply.github.com>
2024-09-20 20:03:42 +0530

    Add files via upload
    Integer type

---
+++ javaPrograms/HelloWorld.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        byte myByte = 127;		//ranges -128 and 127
+        short myShort= 32767;		//range -32768 to 32767
+	int myInt = 2147483647		//range -2147483648 to 2147483647
+	long myLong = 15000000000L;	//range -9223372036854775808 to 9223372036854775807
+	float f1 = 35e3f;	
+	double d1 = 12E4d;
+	
+        System.out.println(myByte);
+        System.out.println(myShort);
+	System.out.println(myInt);
+	System.out.println(myLong);
+	System.out.println(f1);
+	System.out.println(d1);
+    }
+}
